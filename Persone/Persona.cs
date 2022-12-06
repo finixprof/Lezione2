d0de4bcea89179dd7a520a0607447957255392a4
Salvo Finistrella <salvo.finistrella@iispascal.it>
2022-12-06 12:49:05 +0100

    Creata classe persona

---
+++ Persone/Persona.cs
@@ -0,0 +1,50 @@
+﻿
+
+namespace Lezione2.Persone
+{
+    public class Persona
+    {
+        //public string Nome { get; set; } 
+        public string Nome {
+            get {
+                if (Nome == null)
+                    return "Nessuno";
+                return Nome;
+            }
+            set { Nome = value; } 
+        }
+
+
+        private string _cognome;
+        public string Cognome {
+            get { 
+                return _cognome; 
+            }
+            set { 
+                _cognome = value; 
+            }
+        }
+
+        //modalità java
+        //private int _eta;
+
+        //public int GetEta()
+        //{
+        //    return _eta;
+        //}
+
+        //public void SetEta(int value)
+        //{
+        //    _eta = value;
+        //}
+        public int Eta { get; set; }
+
+        //bignorebbe definire che tipo di riscrittura utilizzare new o override
+        public string ToString() 
+        {
+            return $"{Nome} {Cognome} {Eta}";
+        }
+
+
+    }
+}
